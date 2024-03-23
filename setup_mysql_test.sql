f2c54a84610ea2067f5c565703250deccd8e7a43
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-23 11:12:05 +0100

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,11 +4,11 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+-- Grant all privileges on the hbnb_test_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
-GRANT SELECT ON performance_schema.* TO 'hhbnb_test'@'localhost';
+-- Grant SELECT privilege on the performance_schema database to the hbnb_test user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
