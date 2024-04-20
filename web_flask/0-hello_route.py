26af6c366f3faa167d802ca0e86a1b731f2e936b
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-04-20 13:21:43 +0100

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+from flask import Flask
+
+# create the flask application
+app = Flask(__name__)
+
+# define route for homepage
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Run the Flask application on 0.0.0.0:5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,3 @@
+AirBnB clone - Web framework
+
+Python Back-end Webserver Flask
