e710d6b0c7ca043460c7571d6a84a3ca29b0d9fa
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-04-20 13:32:31 +0100

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+"""A script that starts a Flask web application
+The application listens on 0.0.0.0, port 5000.
+Routes: '/'
+Displays 'Hello HBNB!'
+"""
 
 from flask import Flask
 
@@ -10,7 +14,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return "<P>Hello HBNB!</P>"
+    return "Hello HBNB!"
 
 
 """Run the Flask application on 0.0.0.0:5000"""
