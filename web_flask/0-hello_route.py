04e57744d9dbf5f182f5117e77f6b3f2b55e4cc3
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-04-20 13:42:53 +0100

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,16 +7,13 @@ Displays 'Hello HBNB!'
 
 from flask import Flask
 
-"""create the flask application"""
 app = Flask(__name__)
 
-"""define route for homepage"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-"""Run the Flask application on 0.0.0.0:5000"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
